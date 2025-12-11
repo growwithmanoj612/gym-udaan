bcdc332872a82a57ed367c543a68ce96dde59054
bhandarimanoj612 <bhandarimanoj612@gmail.com>
2025-12-11 18:04:01 +0000

    layout commited

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -20,7 +20,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="profile"
         options={{
-          title: "Profile",
+          title: "Home",
           tabBarIcon: ({ color }) => (
             <IconSymbol size={28} name="house.fill" color={color} />
           ),
