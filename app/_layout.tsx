3e6a87a74269c0f7330950c20eb89aae04391e1e
bhandarimanoj612 <bhandarimanoj612@gmail.com>
2025-12-11 21:41:09 +0000

    splash

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,6 +8,8 @@ import { Slot } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
 
+
+
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
