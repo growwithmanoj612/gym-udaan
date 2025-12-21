4a76e0c31f8d093d103e161bc14266af787e1208
bhandarimanoj612 <bhandarimanoj612@gmail.com>
2025-12-21 11:04:37 +0000

    expo push notification

---
+++ components/notification/notifications.ts
@@ -0,0 +1,39 @@
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+import { Platform } from "react-native";
+
+export async function registerForPushNotificationsAsync() {
+  if (!Device.isDevice) {
+    console.warn("Must use physical device for push notifications");
+    return null;
+  }
+
+  const { status: existingStatus } = await Notifications.getPermissionsAsync();
+
+  let finalStatus = existingStatus;
+
+  if (existingStatus !== "granted") {
+    const { status } = await Notifications.requestPermissionsAsync();
+    finalStatus = status;
+  }
+
+  if (finalStatus !== "granted") {
+    alert("Failed to get push token permission!");
+    return null;
+  }
+
+  const token = (await Notifications.getExpoPushTokenAsync()).data;
+
+  console.log("Expo Push Token:", token);
+
+  if (Platform.OS === "android") {
+    await Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+
+  return token;
+}
